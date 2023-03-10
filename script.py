7d7983f8bcb4c4f04ee45e1cf16cb1ad92e2f275
iyalue <2799074006@qq.com>
2023-03-10 00:47:46 +0000

    2

--- script.py
+++ script.py
@@ -5,4 +5,18 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello World'
 
+ @app.route('/login/',methods=['GET','POST'])
+ def login():
+     return 'login page'
+
+ @app.route('/profile/',methods=['GET','POST'])
+ def profile():
+     name = request.args.get('name')
+
+     if not name:
+     # 如果没有name，说明没有登录，重定向到登录页面
+         return redirect(url_for('login'))
+     else:
+         return name
+
 app.run("0.0.0.0",port = "80")
